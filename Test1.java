6e01026c942f527d928194417d1bc662cdcaadf9
Ryan Kuhta <rkuhta@tgm.ac.at>
2018-02-20 10:43:24 +0100

    Added our Test1.java

---
+++ Test1.java
@@ -0,0 +1,13 @@
+
+public class Test1 {
+	public static void gutenTag(){
+		System.out.println("Guten Tag!");
+	}
+	public static void aufWiederSehen(){
+		System.out.print("Auf wiedersehen");
+	}
+	public static void main (String[] args) {
+		gutenTag();
+		aufWiederSehen();
+	}
+}
