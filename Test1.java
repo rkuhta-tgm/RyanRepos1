f11bd034d0dd84cf05e1db4843b902aaacda5dd6
Ryan Kuhta <rkuhta@tgm.ac.at>
2018-02-20 11:15:30 +0100

    Kleine änderung mit einem push

--- Test1.java
+++ Test1.java
@@ -1,13 +1,13 @@
 
 public class Test1 {
-	public static void gutenTag(){
+	public static void gutenTag(String s1){
 		System.out.println("Das hier ist die zweite Version");
-	}
-	public static void aufWiederSehen(){
+	} 
+	public static void aufWiederSehen(String s2){
 		System.out.print("Aus spaﬂ zweite Version");
 	}
 	public static void main (String[] args) {
-		gutenTag();
-		aufWiederSehen();
+		gutenTag("Wow");
+		aufWiederSehen("Lol ist scheisse");
 	}
 }
