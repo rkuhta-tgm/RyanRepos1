22ac482a5c3cf8d3c1e9daaa99b7df0c2ec21ff9
Ryan Kuhta <rkuhta@tgm.ac.at>
2018-02-20 10:48:06 +0100

    We changed out Java file a little and now we recommit

--- Test1.java
+++ Test1.java
@@ -1,10 +1,10 @@
 
 public class Test1 {
 	public static void gutenTag(){
-		System.out.println("Guten Tag!");
+		System.out.println("Das hier ist die zweite Version");
 	}
 	public static void aufWiederSehen(){
-		System.out.print("Auf wiedersehen");
+		System.out.print("Aus spaﬂ zweite Version");
 	}
 	public static void main (String[] args) {
 		gutenTag();
